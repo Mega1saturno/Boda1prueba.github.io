032239befdd49dee91298c03e0f01873baa1a386
Mega1saturno <104749171+Mega1saturno@users.noreply.github.com>
2025-09-13 23:10:38 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ setInterval(updateTimer, 1000);
 // Acción para el botón de confirmación por WhatsApp
 // Funcionalidad para el botón de confirmar con la Novia
 document.getElementById("confirmarNovia").addEventListener("click", function () {
-    const mensaje = "¡Hola Dulce! Confirmo mi asistencia a la boda. ¡Es un honor poder acompañarlos en este día tan especial! ❤️";
+    const mensaje = "¡Hola Dulce! Confirmo mi asistencia a la boda. ¡Es un honor poder acompañarlos en este día tan especial!";
     const numero = "50259868962"; // Reemplaza con el número de la novia
     const url = `https://wa.me/${numero}?text=${encodeURIComponent(mensaje)}`;
     window.open(url, '_blank');
@@ -33,7 +33,7 @@ document.getElementById("confirmarNovia").addEventListener("click", function () 
 
 // Funcionalidad para el botón de confirmar con el Novio
 document.getElementById("confirmarNovio").addEventListener("click", function () {
-    const mensaje = "¡Hola Erick! Confirmo mi asistencia a la boda. ¡Es un honor poder acompañarlos en este día tan especial! ❤️";
+    const mensaje = "¡Hola Erick! Confirmo mi asistencia a la boda. ¡Es un honor poder acompañarlos en este día tan especial!";
     const numero = "50241089461"; // Reemplaza con el número del novio
     const url = `https://wa.me/${numero}?text=${encodeURIComponent(mensaje)}`;
     window.open(url, '_blank');
